04cccd5ad51dbd40e9785fb3d53d9ee01b2381d3
faisalsahak <sahak.faisal@gmail.com>
2017-01-08 01:17:02 -0800

    added signup, signin and profile pages, created the sighn up form, added csrf protection with sessions

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 var express = require('express');
+var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
+var session = require('express-session')
 
 var index = require('./routes/index');
 
-var app = express();
 mongoose.connect('localhost:27017/shopping')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.set('trust proxy', 1)
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -21,6 +22,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  // cookie: { secure: true }
+}));
 
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const Products = require('../models/product');
+const csrf = require('csurf');
+
+const csrfProtection = csrf();
+router.use(csrfProtection)
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   const products = Products.find(function(err, docs){
     res.render('Home', { title: 'Home', products: docs });
   });
 
 });
+
+router.get('/signup', function(req, res){
+  res.render('user/signup', {csrfToken: req.csrfToken()})
+});
+
+router.post('/signup', function(req, res, next){
+  console.log({email: req.body.email, password: req.body.password})
+})
 
 module.exports = router;
